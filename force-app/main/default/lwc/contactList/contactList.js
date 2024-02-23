f64e9ee53a4aaff4dbc9ec00c697dc39bad558ea
ergjus <ergjus.alipaj@gmail.com>
2024-02-22 23:39:10 -0500

    .

--- force-app/main/default/lwc/contactList/contactList.js
+++ force-app/main/default/lwc/contactList/contactList.js
@@ -3,6 +3,8 @@ import FIRSTNAME from '@salesforce/schema/Contact.FirstName';
 import LASTNAME from '@salesforce/schema/Contact.LastName';
 import EMAIL from '@salesforce/schema/Contact.Email';
 import getContacts from '@salesforce/apex/ContactController.getContacts';
+import { reduceErrors } from 'c/ldsUtils';
+
 const COLUMNS = [
     { label: 'First Name', fieldName: FIRSTNAME.fieldApiName, type: 'text' },
     { label: 'Last Name', fieldName: LASTNAME.fieldApiName, type: 'text' },
@@ -12,5 +14,10 @@ const COLUMNS = [
 export default class ContactList extends LightningElement {
     columns = COLUMNS
     @wire (getContacts)
+    
     contacts;
+    
+    get errors(){
+        return (this.contacts.error) ? reduceErrors(this.contacts.error) : [];
+    }
 }
